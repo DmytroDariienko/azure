56fe5294541590f423480b8ecadc63ee0114facb
DmytroDariienko <52741959+DmytroDariienko@users.noreply.github.com>
2019-08-08 17:04:41 +0300

    Create ssl_dom.ps1

---
+++ ssl_dom.ps1
@@ -0,0 +1,17 @@
+$resourcegroupname = "tomcat-a"
+$pfxPath = "/home/dmytro/ssl.pfx"
+$pfxPassword = $Args[0]
+
+Write-Host "Configure a CNAME record that maps www.ddaritestapp.tk to tomcat-app-us.azurewebsites.net"
+Read-Host "Press [Enter] key when ready ..."
+
+Set-AzWebApp -Name "tomcat-app-us" -ResourceGroupName $resourcegroupname -HostNames @("www.ddaritestapp.tk","tomcat-app-us.azurewebsites.net")
+
+New-AzWebAppSSLBinding -WebAppName "tomcat-app-us" -ResourceGroupName $resourcegroupname -Name "www.ddaritestapp.tk" -CertificateFilePath $pfxPath -CertificatePassword $pfxPassword -SslState SniEnabled
+
+Write-Host "Configure a CNAME record that maps ddaritestapp.tk to tomcat-app-eu.azurewebsites.net"
+Read-Host "Press [Enter] key when ready ..."
+
+Set-AzWebApp -Name "tomcat-app-eu" -ResourceGroupName $resourcegroupname -HostNames @("ddaritestapp.tk","tomcat-app-eu.azurewebsites.net")
+
+New-AzWebAppSSLBinding -WebAppName "tomcat-app-eu" -ResourceGroupName $resourcegroupname -Name "ddaritestapp.tk" -CertificateFilePath $pfxPath -CertificatePassword $pfxPassword -SslState SniEnabled
